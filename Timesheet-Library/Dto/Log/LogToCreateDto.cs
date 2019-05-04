8548d2cb256f37d082836d946d21b7981c1b8cf2
Tim Van den Borre <tim.van.den.borre@student.ehb.be>
2019-05-04 13:10:11 +0200

    Writing api calls for Log + last call for Users + testing

---
+++ Timesheet-Library/Dto/Log/LogToCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timesheet_Library.Dto.Log
+{
+    class LogToCreateDto
+    {
+        public int UserID { get; set; }
+        public int ProjectID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime StopTime { get; set; }
+        public string Description { get; set; }
+    }
+}
