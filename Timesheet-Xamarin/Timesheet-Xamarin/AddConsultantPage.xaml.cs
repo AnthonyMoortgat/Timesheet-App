7c8cea141adaabe14f2f50f48e78ab532015626c
Tim Van den Borre <tim.van.den.borre@student.ehb.be>
2019-05-16 14:11:43 +0200

    Changes Consultant page(back button

--- Timesheet-Xamarin/Timesheet-Xamarin/AddConsultantPage.xaml.cs
+++ Timesheet-Xamarin/Timesheet-Xamarin/AddConsultantPage.xaml.cs
@@ -16,5 +16,10 @@ namespace Timesheet_Xamarin
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
 }
