4a724f3c19fe2b67fd53be7bd16e9735ee269f0e
michaeldegauquier <michael.de.gauquier@hotmail.com>
2019-05-21 13:27:17 +0200

    Error fixed of 'function not found'

--- Timesheet-Xamarin/Timesheet-Xamarin/ConsultantListProjectPage.xaml.cs
+++ Timesheet-Xamarin/Timesheet-Xamarin/ConsultantListProjectPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Timesheet_Library.Dto;
+using Timesheet_Library.Dto.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,85 @@ namespace Timesheet_Xamarin
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ConsultantListProjectPage : ContentPage
 	{
-		public ConsultantListProjectPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+        UserServices userServices = new UserServices();
+        ProjectServices projectServices = new ProjectServices();
+        CompanyServices companyServices = new CompanyServices();
+        string idUser = Application.Current.Properties["IdUser"].ToString();
+        string projectId = Application.Current.Properties["projectid"].ToString();
+        //Company ID
+
+        public ObservableCollection<string> UserCollection = null;
+
+        //Consultants met Key
+        Dictionary<int, string> consultantWithKey = new Dictionary<int, string>();
+
+        public ConsultantListProjectPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            try
+            {
+                //Haalt alle users op van het project
+                List<UserDto> consultants = await companyServices.GetUsersFromCompanyByIdAsync(1); //Moet Get users from project met projectid
+
+                //Steekt alle projecten in ProjectList (Picker)
+                AddUserToConsultantList(UserCollection, consultants);
+            }
+            catch (Exception)
+            {
+                LabelNoUsers.IsEnabled = true;
+                LabelNoUsers.IsVisible = true;
+            }
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            string consultantToEdit = Consultantlist.SelectedItem.ToString();
+            int userid = 0;
+
+            //Zoekt id van de user
+            foreach (var c in consultantWithKey)
+            {
+                if (c.Value == consultantToEdit)
+                {
+                    userid = c.Key;
+                }
+            }
+
+            Consultantlist.SelectedItem = null;
+            //await Navigation.PushModalAsync(new EditConsultantPage(userid));
+        }
+
+        //Users toevoegen aan Consultantlist (ListView)
+        private void AddUserToConsultantList(ObservableCollection<string> UserCollection, List<UserDto> usersDto = null)
+        {
+            UserCollection = new ObservableCollection<string>();
+            //Users(email en id) in Dictionary steken
+            foreach (var user in usersDto)
+            {
+                consultantWithKey.Add(user.ID, user.Email);
+            }
+
+            //Dictionary in Consultantlist(ListView) steken
+            foreach (var user in consultantWithKey)
+            {
+                UserCollection.Add(user.Value.ToString());
+            }
+
+            //Dictionary in Consultantlist(ListView) steken
+            Consultantlist.ItemsSource = UserCollection;
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new EditProjectPage();
+        }
+    }
 }
